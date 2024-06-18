0f79cd185c0452ab4a758ae4b337a823803ba9b7
swje <evokbacus@gmail.com>
2024-06-18 17:34:09 +0900

    first commit

---
+++ file_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>#[[$Title$]]#</title>
+</head>
+<body>
+#[[$END$]]#
+file_1.php
+</body>
+</html>
+
+
+<?php
+echo "file_1.php";
+
+
+
+
+
