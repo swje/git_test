fbabe5f9f40ee585214f21562413e59676dc883a
swje <evokbacus@gmail.com>
2024-06-18 17:55:45 +0900

    2nd commit

---
+++ file_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>#[[$Title$]]#</title>
+</head>
+<body>
+#[[$END$]]#
+file_2.php
+</body>
+</html>
+
+
+<?php
+echo "file_2.php";
+
+
+
+
+
